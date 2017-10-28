8ca0463de008ca7b8f43b432e41816e00b9a55be
wujian <wujian@smartisan.com>
2017-10-28 13:05:10 +0800

    4.Binary-Tree: traversing binary trea

--- 4.Tree/BinaryTree.h
+++ 4.Tree/BinaryTree.h
@@ -13,3 +13,19 @@ typedef struct BiTnode
 	ElemType data;
     struct BiTnode *lchild, *rchild;
 } BiTNode, *BiTree;
+
+/*
+traversing binary trea
+前序遍历 中左右 ABDH I EJ CFK G
+中序遍历 左中右 HDI B EJ A FK C G A
+后序遍历 叶子开始 HID JE B KF G C A
+层序遍历        A BC DEFG HIJK
+
+          A
+       ↙    ↘
+     B         C
+   ↙  ↘     ↙   ↘
+  D    E    F     G
+↙  ↘    ↘    ↘
+H   I    J     K
+*/
